fe95ac1d7fe32352ae534ca53a357e953e1dfe1f
Florian Sauerland <s7flsaue@uni-bonn.de>
2017-02-03 14:43:39 +0100

    replace malencoded characters in csvReader
    Signed-off-by: Florian Sauerland <s7flsaue@uni-bonn.de>

--- src/model/CsvReader.java
+++ src/model/CsvReader.java
@@ -2,25 +2,31 @@ package model;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class CsvReader {
 	public static List<PointDescriptor> readFile(String filename){
 		LinkedList<PointDescriptor> list = new LinkedList<PointDescriptor>();
-		
+
 		File file = new File(filename);
-		FileReader fr;
+		FileInputStream fis;
+		InputStreamReader isr;
+		BufferedReader br;
 		try {
-			fr = new FileReader(file);
-		} catch (FileNotFoundException e) {
+			fis = new FileInputStream(file);
+			isr = new InputStreamReader(fis, "Cp1252");	
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
 			return new LinkedList<PointDescriptor>();
 		}
-		BufferedReader br = new BufferedReader(fr);
+		br = new BufferedReader(isr);
 		try {
 			nextLine: for(String line = br.readLine(); line != null; line = br.readLine()){
 				try {
@@ -29,6 +35,12 @@ public class CsvReader {
 					double y = Double.parseDouble(args[2]);
 					double height = Double.parseDouble(args[3]);
 					double width = Double.parseDouble(args[4]);
+//					for(char i : args[0].toCharArray()){
+//						System.out.println(Character.toString(i) + ": \\u" + Integer.toHexString(i | 0x10000).substring(1) );
+//					}
+//					System.out.println(args[0]);
+					args[0] = args[0].replace('\ufffd', '\u00FC'); // wer auch immer die Testdaten gemacht hat: "\ufffd"? so kodiert man das ue doch nicht!
+//					System.out.println(args[0]);
 					list.add(new PointDescriptor(args[0], y, x, width, height));
 				} catch (NumberFormatException e) {
 					continue nextLine;
@@ -38,7 +50,6 @@ public class CsvReader {
 			e.printStackTrace();
 			try {
 				br.close();
-				fr.close();
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
@@ -46,7 +57,6 @@ public class CsvReader {
 		}
 		try {
 			br.close();
-			fr.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
